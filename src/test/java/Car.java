2781f633a92238da383de1e4d7880dd41b6dcd4b
sunghun <nokdonwithkj211@gmail.com>
2023-08-04 11:22:23 +0900

    docs(README.md), feat(난수 생성): 기능 요구사항 작성, 난수를 생성
    다음 부터는 하나씩 push 하도록

---
+++ src/test/java/Car.java
@@ -0,0 +1,20 @@
+
+
+public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    // 추가 기능 구현하는 구간
+    // 앞으로 전진한다
+    public void moveForward(){
+        this.position++;
+    }
+
+    // 멈춘다 ( 전진하지 않는 경우라고 생각해서 메서드를 따로 추가하지는 않았다 )
+
+
+}
---
+++ src/test/java/controller/GameController.java
@@ -0,0 +1,27 @@
+package controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+// 메서드 별 15라인 넘기지 말기
+// else 사용하지 말기
+public class GameController {
+
+    public List<Integer> generateRandomNumber(int count){
+        // 인자 값 만큼 랜덤 숫자를 생성하는 메서드를 의미
+        List<Integer> randomNumbers = new ArrayList<>(count);
+        Random random = new Random();
+        random.setSeed(System.currentTimeMillis());
+
+        for( int i = 0 ; i < count ; i++ ) {
+            int randomNumber = random.nextInt(10);
+            randomNumbers.set(i, randomNumber);
+        }
+
+        return randomNumbers;
+    }
+
+    
+
+}
